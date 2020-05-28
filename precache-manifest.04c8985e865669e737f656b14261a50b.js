244cd284340ce3d28804a80fc3f8fac2e607a344
unknown <bmalkes@gmail.com>
2020-05-28 16:08:31 +0300

    Updates

---
+++ precache-manifest.04c8985e865669e737f656b14261a50b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47cfddfd24987799cc1f4829bfc4d780",
+    "url": "/BmAlkes/react-weather-app/index.html"
+  },
+  {
+    "revision": "9fe4d27d33ab4279be2f",
+    "url": "/BmAlkes/react-weather-app/static/css/main.425f534c.chunk.css"
+  },
+  {
+    "revision": "5c50521b05e90dabe252",
+    "url": "/BmAlkes/react-weather-app/static/js/2.f6d751da.chunk.js"
+  },
+  {
+    "revision": "9fe4d27d33ab4279be2f",
+    "url": "/BmAlkes/react-weather-app/static/js/main.82e7a615.chunk.js"
+  },
+  {
+    "revision": "60315b5b379d4fc23bfd",
+    "url": "/BmAlkes/react-weather-app/static/js/runtime-main.0a0a3740.js"
+  },
+  {
+    "revision": "448a52622f6ff192bc0f47d68fc704ff",
+    "url": "/BmAlkes/react-weather-app/static/media/1.448a5262.png"
+  },
+  {
+    "revision": "56ad48ec770b64e576b36bd53d856068",
+    "url": "/BmAlkes/react-weather-app/static/media/14.56ad48ec.png"
+  },
+  {
+    "revision": "2477303a8baadab67a5cfdfbb2a2280a",
+    "url": "/BmAlkes/react-weather-app/static/media/18.2477303a.png"
+  },
+  {
+    "revision": "39ec473924f0da17b426d467a5b85248",
+    "url": "/BmAlkes/react-weather-app/static/media/6.39ec4739.png"
+  },
+  {
+    "revision": "5cb0a006a6f7e54ceb14485a5ad7b3e8",
+    "url": "/BmAlkes/react-weather-app/static/media/7.5cb0a006.png"
+  }
+]);
